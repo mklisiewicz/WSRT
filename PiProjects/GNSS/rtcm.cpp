5ae94645a864d35456e200a12960367ee93b09c3
Mateusz Klisiewicz <129537461+mklisiewicz@users.noreply.github.com>
2023-08-08 23:13:32 +0200

    Update rtcm.cpp

--- PiProjects/GNSS/rtcm.cpp
+++ PiProjects/GNSS/rtcm.cpp
@@ -1,13 +1,114 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <fstream>
 #include <linux/i2c-dev.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <chrono>
+
 
 #define RCV_ADDR 0x42
-#define CHUNK_SIZE 128
+#define RTCM3_PREAMBLE 0xD3
+
+const uint8_t DISABLE_UART1[] = {
+    0xB5, 0x62, 0x06, 0x00, 0x14, 0x00,
+    0x01, 0x00, 0x00, 0x00, 0xD0, 0x08,
+    0x00, 0x00, 0x00, 0x96, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x89, 0x46};
+
+const uint8_t DISABLE_UART2[] = {
+    0xB5, 0x62, 0x06, 0x00, 0x14, 0x00, 
+    0x02, 0x00, 0x00, 0x00, 0xD0, 0x08, 
+    0x00, 0x00, 0x00, 0x96, 0x00, 0x00, 
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+    0x00, 0x00, 0x8A, 0x5A};
+
+
+const uint8_t DISABLE_USB[] = {
+    0xB5, 0x62, 0x06, 0x00, 0x14, 0x00,
+    0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+    0x00, 0x00, 0x1D, 0x84};
+
+const uint8_t DISABLE_SPI[] = {
+    0xB5, 0x62, 0x06, 0x00, 0x14, 0x00, 
+    0x04, 0x00, 0x00, 0x00, 0x00, 0x32, 
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+    0x00, 0x00, 0x50, 0x86};
+
+const uint8_t SET_RATE_UTC[] = {
+    0xB5, 0x62, 0x06, 0x08, 0x06, 0x00,
+    0x19, 0x00, 0x01, 0x00, 0x00, 0x00,
+    0x2E, 0x4E
+};
+
+const uint8_t SET_RATE_GPS[] = {
+    0xB5, 0x62, 0x06, 0x08, 0x06, 0x00,
+    0x19, 0x00, 0x01, 0x00, 0x01, 0x00,
+    0x2F, 0x50
+};
+
+const uint8_t SET_RATE_GAL[] = {
+    0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 
+    0x19, 0x00, 0x01, 0x00, 0x02, 0x00, 
+    0x30, 0x52
+};
+
+const uint8_t SET_RATE_GLO[] = {
+    0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 
+    0x19, 0x00, 0x01, 0x00, 0x03, 0x00, 
+    0x31, 0x54
+};
+
+const uint8_t SET_RATE_BDS[] = {
+    0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 
+    0x19, 0x00, 0x01, 0x00, 0x04, 0x00, 
+    0x32, 0x56,
+};
+
+const uint8_t SET_I2C_OUT[] = {
+    
+};
+
+const uint8_t SET_I2C_IN[] = {
+
+};
+
+const uint8_t* commands[] = {
+    DISABLE_UART1,
+    DISABLE_UART2,
+    DISABLE_USB,
+    DISABLE_SPI,
+    SET_RATE_UTC,
+    SET_RATE_GPS,
+    SET_RATE_GAL,
+    SET_RATE_GLO,
+    SET_RATE_BDS
+};
+
+
+void sendConfig(int i2cHandle) {
+    for (int i = 0; i < sizeof(commands); i++){
+        write(i2cHandle, commands[i], sizeof(commands));
+    }
+}
+
+unsigned long millis() {
+    return std::chrono::duration_cast<std::chrono::milliseconds>(
+               std::chrono::high_resolution_clock::now().time_since_epoch()
+           ).count();
+}
+
+void write_to_file(uint8_t *ptr, size_t len) {
+    std::ofstream output_file("/home/mateusz/gps/nmea_messages.txt", std::ios::app);
+    output_file.write((char*)ptr, len);
+    output_file.close();
+}
 
 int i2cOpen() {
     int i2cHandle = open("/dev/i2c-1", O_RDWR);
@@ -30,54 +131,58 @@ void i2cWrite(const std::vector<uint8_t>& data, int i2cHandle) {
     }
 }
 
-void sendRTCM(const std::vector<uint8_t>& data, int i2cHandle) {
-    i2cSetAddress(i2cHandle);
-    i2cWrite(data, i2cHandle);
-}
-
 void readRTCM(int i2cHandle) {
-    std::vector<uint8_t> data_chunk(CHUNK_SIZE);
-    std::cin.read(reinterpret_cast<char*>(data_chunk.data()), CHUNK_SIZE);
-    size_t data_read = std::cin.gcount();
-    if (data_read > 0) {
-        std::vector<uint8_t> data(data_chunk.begin(), data_chunk.begin() + data_read);
-        sendRTCM(data, i2cHandle);
+    std::vector<uint8_t> rtcm_data;
+    uint8_t rtcm_byte;
+    uint8_t rtcm_bytes[1024];
+    if (std::cin.get(reinterpret_cast<char&>(rtcm_byte)) && rtcm_byte == RTCM3_PREAMBLE) {
+        std::string line;
+        std::getline(std::cin, line, '\n');  
+        std::cout << sizeof(line) << std::endl;
+        for (char c : line) {
+            rtcm_data.push_back(static_cast<uint8_t>(c));
         }
+
+        i2cWrite(rtcm_data, i2cHandle);
+    }
 }
 
 void readNMEA(int i2cHandle) {
-    std::string received_data;
+    uint8_t received_data[96];
     uint8_t received_byte;
-    bool save = false;
-    if (read(i2cHandle, &received_byte, 1) != 1) {
-        std::cerr << "Failed to read from I2C." << std::endl;
-    }
-    else if (received_byte == '$'){
-        save = true;
-    }
-    if (save){
-        received_data.push_back(received_byte);
-        if (received_byte == '\n') {
-            // Process and save NMEA message to a file
-            std::ofstream output_file("/home/mateusz/gps/nmea_messages.txt", std::ios::app);
-            if (output_file.is_open()) {
-                output_file << received_data;
-                output_file.close();
-            } else {
-                std::cerr << "Failed to open output file." << std::endl;
-            }
-            received_data.clear();
-            save = false;
+    int i = 0;
+    static unsigned long startTime = 0; 
+    unsigned long endTime = 0; 
+
+    if (read(i2cHandle, &received_byte, 1) == 1 && received_byte == '$') {
+        while(read(i2cHandle, &received_byte, 1) == 1 && received_byte != '\n') {
+            received_data[i] = received_byte;
+            i++;
         }
+        received_data[i] = '\n';
+        std::cout << i << std::endl;
+        write_to_file(received_data, i+1);
     }
 }
 
-int main() {
+int setup() {
+    unsigned long startTime = millis();
     int i2cHandle = i2cOpen();
     i2cSetAddress(i2cHandle);
+    sendConfig(i2cHandle);
+    while (millis() - startTime < 1000)
+    {
+        std::cout << "Waiting for GPS to start..." << std::endl;
+    }
+    return i2cHandle;
+}
+
+int main() {
+    int i2cHandle = setup();
+
     while (true) {
-        //readRTCM(i2cHandle);
         readNMEA(i2cHandle);
+        readRTCM(i2cHandle);
     }
     close(i2cHandle);
     return 0;
